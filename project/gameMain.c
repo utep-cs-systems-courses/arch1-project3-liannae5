2787d4e6444d43ba325202614a4cfbfee65a763c
liannae5 <laestrada5@miners.utep.edu>
2022-05-06 11:26:02 -0600

    got rid of extra files

--- project/gameMain.c
+++ project/gameMain.c
@@ -3,7 +3,7 @@
 #include "lcdutils.h"
 #include "lcddraw.h"
 #include "switches.h"
-
+#include "buzzer.h"
 
 void update_shape();
 int BG_COLOR = COLOR_BLUE;
@@ -13,6 +13,7 @@ void main()
   P1OUT |= LED;
   configureClocks();
   lcd_init();
+  buzzer_init();
   switch_init();
   enableWDTInterrupts();      /**< enable periodic interrupt */
   or_sr(0x8);              /**< GIE (enable interrupts) */
@@ -27,59 +28,99 @@ void main()
     P1OUT |= LED;/* led on */
   }
 }
-
+static unsigned char row = screenHeight / 2, col = screenWidth / 2;
+static char blue = 31, green = 0, red = 31;
+static unsigned char step = 0;
+static char state = 0;
+static char width= 10;
+static int color = COLOR_RED;
     
 void update_shape()
 {
-  static unsigned char row = screenHeight / 2, col = screenWidth / 2;
+  /*  static unsigned char row = screenHeight / 2, col = screenWidth / 2;
   static char blue = 31, green = 0, red = 31;
   static unsigned char step = 0;
   static char state = 0;
-  static char width= 30;
-  static int color = COLOR_RED;
-  // printf("Screen height is: %d\n",screenHeight);
-  // printf("Screen width is: %d\n",screenWidth);
+  static char width= 10;
+  static int color = COLOR_RED;*/
   if (switches & SW4) return;
+  down_arrow();
+  // nf_intro();
   if(switches & SW3){
-    color = COLOR_GREEN;
+    square_line();
   }
-  
-//four corner appear/dissapear
-  switch(state){
+}
+
+void down_arrow(){
+  static char state = 0;
+    switch(state){
   case 0:
-    clearScreen(COLOR_BLUE);
-    // fillRectangle(0,screenHeight-30,width,width,COLOR_BLUE);
-    fillRectangle(0,0,width,width,color);
+    // clearScreen(COLOR_BLUE);
+    fillRectangle(col-width,screenHeight/3-width,width,width,COLOR_BLUE);
+    fillRectangle(0,0,width,width,COLOR_RED);
     state = 1;
-    color = COLOR_RED;
     break;
   case 1:
-    clearScreen(COLOR_BLUE);
-    // fillRectangle(0,0,width,width,COLOR_BLUE);
-    fillRectangle(screenWidth-width,0,width,width,color);
+    // clearScreen(COLOR_BLUE);
+    fillRectangle(0,0,width,width,COLOR_BLUE);
+    fillRectangle(screenWidth-width,0,width,width,COLOR_RED);
     state = 2;
-    color = COLOR_RED;
     break;
   case 2:
-    clearScreen(COLOR_BLUE);
-    // fillRectangle(screenWidth-width,0,width,width,COLOR_BLUE);
-    fillRectangle(screenWidth-30,screenHeight-30,width,width,color);
-    state = 3;
-    color = COLOR_RED;
-    break;
-  case 3:
-    clearScreen(COLOR_BLUE);
-    
-    // fillRectangle(screenWidth-30,screenHeight-30,width,width,COLOR_BLUE);
-    fillRectangle(0,screenHeight-30,width,width,color);
-    state = 0;
-    color = COLOR_RED;
-    break;
-  default:
+    // clearScreen(COLOR_BLUE);
+    fillRectangle(screenWidth-width,0,width,width,COLOR_BLUE);
+    fillRectangle(col-width,screenHeight/3-width,width,width,COLOR_RED);
     state = 0;
     break;
     }
-      
-
-  
 }
+ void square_line(){
+   
+   fillRectangle(0,row,width+(secondsW%screenWidth),width,COLOR_BLUE);
+   fillRectangle(secondsW%screenWidth,row,width,width,COLOR_GREEN);
+ }
+void nf_intro(){
+  static char nfi_state = 0;
+  static int f_note = 698;
+  static int e_note = 659;
+  static int d_note = 587;
+  static int a_note = 440;
+  static int d_flat_note = 554;
+  static int b_flat_note = 466;
+  switch(nfi_state){
+    case 0:
+      buzzer_set_period(f_note);
+      nfi_state = 1;
+      break;
+    case 1:
+      buzzer_set_period(e_note);
+      nfi_state = 2;
+      break;
+    case 2:
+      buzzer_set_period(d_note);
+      nfi_state = 3;
+      break;
+    case 3:
+      buzzer_set_period(d_flat_note);
+      nfi_state = 4;
+      break;
+    case 4:
+      buzzer_set_period(d_note);
+      nfi_state = 5;
+      break;
+    case 5:
+      buzzer_set_period(d_flat_note);
+      nfi_state = 6;
+      break;
+    case 6:
+      buzzer_set_period(a_note);
+      nfi_state = 7;
+      break;
+    case 7:
+      buzzer_set_period(b_flat_note);
+      nfi_state = 0;
+      break;
+  }
+  
+}  
+
--- project/switches_c.c
+++ project/switches_c.c
@@ -52,6 +52,7 @@ void wdt_c_handler()
   static int sec1Count = 0;
   if (sec2Count++ >= 125) {/* 2/sec */
     sec2Count = 0;
+    secondsW++;
     // current_color = (current_color+1) % NUM_SQCOLORS;
     // redrawScreen = 1;
   }
@@ -62,7 +63,7 @@ void wdt_c_handler()
       clearScreen(COLOR_PINK);
       return;
       }*/
-    secondsW++;
+    // secondsW++;
     // current_position = (current_position+1) % NUM_POSITIONS;
     redrawScreen = 1;
   }
